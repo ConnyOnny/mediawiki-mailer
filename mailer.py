79ede75042bf7e9409f655390edfca70c7684c72
Constantin Berhard <constantin@exxxtremesys.lu>
2015-01-15 11:44:13 +0100

    email.utils to the rescue

--- mailer.py
+++ mailer.py
@@ -3,31 +3,23 @@ import conf
 import re
 import itertools
 import my_logging
+import email.utils
 
 logger = my_logging.getLogger(__name__)
 
-_extractor_match_ltgt = re.compile("^[^<>]*<([^<>@]+@[^<>@]+)>$")
-_match_mail = re.compile("[^<>@]+@[^<>@]+")
-def extract_mail (s):
-	"""
-	name <mail@example.com> => mail@example.com
-	mail@example.com => mail@example.com
-	"""
-	match = _extractor_match_ltgt.match(s)
-	if match:
-		return match.group(1)
-	elif _match_mail.match(s):
-		return s
-	else:
-		logger.warning('Could not verify email address "%s"'%s)
-		# change nothing and hope for the best
-		return s
+_DST_FIELDS = ["to","cc","resent-to","resent-cc"]
 
-def _get_by_regex (msg, regexp):
+def get_mail_destination_addresses (msg):
+	dsts = []
+	for field in _DST_FIELDS:
+		dsts += msg.get_all(field,[])
+	adrs_pairs = email.utils.getaddresses(dsts)
+	return list(map(lambda x:x[1],adrs_pairs))
+
+def _get_first_group_if_match (strings, regexp):
 	matching = []
-	for adr in itertools.chain(msg.get_all("To",[]), msg.get_all("Cc",[])):
-		adr = extract_mail(adr)
-		match = regexp.match(adr)
+	for s in strings:
+		match = regexp.match(s)
 		if match:
 			matching.append(match.group(1))
 	return matching
@@ -39,10 +31,12 @@ def _adr_unescape(adr):
 	return adr
 
 def get_lists (msg):
-	return list(map(_adr_unescape,_get_by_regex(msg,re.compile(conf.mailing_list_regex))))
+	addresses = get_mail_destination_addresses(msg)
+	return list(map(_adr_unescape,_get_first_group_if_match(addresses,re.compile(conf.mailing_list_regex))))
 
 def get_users (msg):
-	return list(map(_adr_unescape,_get_by_regex(msg,re.compile(conf.individual_mail_regex))))
+	addresses = get_mail_destination_addresses(msg)
+	return list(map(_adr_unescape,_get_first_group_if_match(addresses,re.compile(conf.individual_mail_regex))))
 
 def add_listinfo (msg):
 	msg.add_header("List-Unsubscribe",conf.unsubscribe_header)
