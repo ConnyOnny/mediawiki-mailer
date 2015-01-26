e55407a95586c0d391947ac68322034c37c449f7
Constantin Berhard <constantin@exxxtremesys.lu>
2015-01-26 15:39:19 +0100

    try to reduce duplicate mails
    issue #1

--- mysqltable.py
+++ mysqltable.py
@@ -6,6 +6,7 @@ import conf
 logger = my_logging.getLogger(__name__)
 
 class MailTable:
+	# TODO: provide support for "with MailTable() as mt:" for clean closing of the connection
 	def __init__(self):
 		self._conn = cymysql.connect(host=conf.mysql_server, user=conf.mysql_user, passwd=conf.mysql_password, db=conf.mysql_database, charset='utf8')
 	def get_mail(self,name):
@@ -14,6 +15,7 @@ class MailTable:
 		rows = cur.fetchall()
 		assert len(rows) == 0 or len(rows) == 1
 		if len(rows) == 0:
+			logger.warning("User not found: %s"%name)
 			return None
 		else:
 			return rows[0][0].decode('utf-8')
